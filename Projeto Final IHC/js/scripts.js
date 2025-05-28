6c8e256ec0243c8a59902d2b0971eaee7d1e5ac6
roger.arraz <roger.arraz@sempreceub.com>
2025-05-28 10:36:35 -0300

    Prototipo V2

---
+++ Projeto Final IHC/js/scripts.js
@@ -0,0 +1,6 @@
+// JavaScript customizado para o site Loki
+
+// Exemplo de inicialização após o carregamento do DOM
+document.addEventListener('DOMContentLoaded', () => {
+  // Aqui você pode adicionar código JS específico, se necessário
+});
